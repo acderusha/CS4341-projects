6fab6255bdd9e955eb8fc8eceac842f059bb67f7
Andrew DeRusha <acderusha@wpi.edu>
2019-02-24 15:44:00 -0500

    goes to the goal but removing walls

--- Bomberman/group21/testcharacter2.py
+++ Bomberman/group21/testcharacter2.py
@@ -22,10 +22,14 @@ class TestCharacter(CharacterEntity):
 
         # Get all possible directions fro agent
         allDirections = self.getAllDirections(wrld, start)
+        allSpaces = self.getAllSpaces(wrld, start)
 
         # Find the current best move for the agent
-        bestScoreMove = self.scoreMoves(wrld, start, goal, allDirections)
-        self.move(bestScoreMove[0], bestScoreMove[1])
+        bestScoreMove = self.scoreMoves(wrld, start, goal, allDirections, allSpaces)
+        if(bestScoreMove == 'B'):
+            self.place_bomb()
+        else:
+            self.move(bestScoreMove[0], bestScoreMove[1])
 
         # Go to the goal state if the path leads to a space next to it. ie Terminal Test
         self.goToGoal(start, goal)
@@ -41,16 +45,45 @@ class TestCharacter(CharacterEntity):
     #
     # RETRUNS: the best move for the agent
     #
-    def scoreMoves(self, wrld, start, goal, allDirections):
+    def scoreMoves(self, wrld, start, goal, allDirections, allSpaces):
         enemies = self.getEnemy(wrld)
+        bombs = self.getBomb(wrld)
+        explosions = self.getExplosion(wrld)
 
         # Gets the move recommended by A*
         a_star_move = self.get_a_star_move(wrld, start, goal)
+        bestMove = (a_star_move[0], a_star_move[1])
 
-        bestMove = -1
+        # Deterermine if agent can move that direction
+        if (a_star_move not in allDirections and len(bombs) != 1 and len(explosions) == 0):
+            bestMove = 'B'
+            return bestMove
+
         highestScore = -1
         for i in range(len(allDirections)):
             livingScore = abs(wrld.time)
+
+            bombScore = 0
+            bombX = 0
+            bombY = 0
+            for bombLoc in bombs:
+                if(allSpaces[i][0] == bombLoc[0]):
+                    bombX = -5
+                if (allSpaces[i][1] == bombLoc[1]):
+                    bombY = -5
+
+                bombScore = bombScore + bombX + bombY
+
+            explosionScore = 0
+            explosionX = 0
+            explosionY = 0
+            for explosionLoc in explosions:
+                if (allSpaces[i][0] == explosionLoc[0]):
+                    explosionX = -5
+                if (allSpaces[i][1] == explosionLoc[1]):
+                    explosionY = -5
+
+                explosionScore = explosionScore + explosionX + explosionY
 
             enemyScore = 0
             for enemyLoc in enemies:
@@ -58,15 +91,15 @@ class TestCharacter(CharacterEntity):
                 futureY = (self.y + allDirections[i][1])
 
                 enemyDis = math.sqrt((enemyLoc[0] - futureX) ** 2 + (enemyLoc[1] - futureY) ** 2)
-                if(enemyDis < 5):
-                    enemyScore = enemyScore - ((5 - enemyDis) * 4)
+                if (enemyDis < 4):
+                    enemyScore = enemyScore - ((4 - enemyDis) * 6)
 
             a_star_score = 0
             if(a_star_move == allDirections[i]):
                 a_star_score = 5
 
-            totalScore = livingScore + a_star_score + enemyScore
-            print(totalScore)
+            totalScore = livingScore + a_star_score + bombScore + explosionScore + enemyScore
+            print(allDirections[i], totalScore)
             if(totalScore > highestScore):
                 highestScore = totalScore
                 bestMove = allDirections[i]
@@ -91,6 +124,42 @@ class TestCharacter(CharacterEntity):
                     enemies.append((x, y))
 
         return enemies
+
+    # Gets the locations of the bomb location
+    #
+    # PARAM: [world] wrld: the current state of the world
+    #
+    # RETURNS: [list [entities]] bombs: a list of bombs
+    #
+    def getBomb(self, wrld):
+        bombs = []
+
+        x = 0
+        y = 0
+        for y in range(wrld.height()):
+            for x in range(wrld.width()):
+                if (wrld.bomb_at(x, y)):
+                    bombs.append((x, y))
+
+        return bombs
+
+    # Gets the locations of the explosion location
+    #
+    # PARAM: [world] wrld: the current state of the world
+    #
+    # RETURNS: [list [entities]] explosions: a list of explosions
+    #
+    def getExplosion(self, wrld):
+        explosions = []
+
+        x = 0
+        y = 0
+        for y in range(wrld.height()):
+            for x in range(wrld.width()):
+                if (wrld.explosion_at(x, y)):
+                    explosions.append((x, y))
+
+        return explosions
 
 
     # Goes to the goal / exit
@@ -334,9 +403,34 @@ class TestCharacter(CharacterEntity):
         a_star_path = self.a_star_search(wrld, start, goal)
 
         direction = self.findPath(start, goal, wrld, a_star_path)
+
+        # If no path found, then move towards goal
         if(len(direction[0]) == 0):
-            return defaultMove
+            return self.moveTowardsGoal(start, goal)
 
         nextMove = direction[len(direction) - 2]
         a_star_move = (nextMove[0] - self.x, nextMove[1] - self.y)
         return a_star_move
+
+    # Returns the move that leads the agent closer to the goal
+    #
+    # PARAM: [[int, int], [int, int]] wrld: [start.x, start.y]: the x and y coordinated the agent is located at
+    #                                       [goal.x, goal.y]: the x and y coordinated of the goal / exit
+    #
+    # RETURNS: [int, int]: (x, y): the directions towards th goal
+    #
+    def moveTowardsGoal(self, start, goal):
+        x = 0
+        y = 0
+        # Get x component
+        if (start[0] > goal[0]):
+            x = -1
+        if (start[0] < goal[0]):
+            x = 1
+        # Get y component
+        if (start[1] > goal[1]):
+            y = -1
+        if (start[1] < goal[1]):
+            y = 1
+
+        return (x, y)
